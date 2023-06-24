e44b0577d9619b827afab02234cc89c6914ebe54
Ubaid Zada <ubaidzada213@gmail.com>
2023-06-24 17:11:53 +0500

    Assignment1

---
+++ Software house 1.sql
@@ -0,0 +1,20 @@
+SELECT * FROM softwarehouse.emp; -- point a
+
+select * from emp where job = "MANAGER"; -- point b
+
+-- Option d start
+update emp set hiredate="2009-05-27" where id="7369";
+update emp set hiredate="2010-05-27" where id="7499";
+update emp set hiredate="2011-05-27" where id="7521";
+update emp set hiredate="2012-05-27" where id="7566";
+update emp set hiredate="2013-05-27" where id="7654";
+update emp set hiredate="2014-05-27" where id="7698";
+update emp set hiredate="2015-05-27" where id="7782";
+update emp set hiredate="2016-05-27" where id="7788";
+update emp set hiredate="2017-05-27" where id="7839";
+update emp set hiredate="2018-05-27" where id="7844";
+update emp set hiredate="2019-05-27" where id="7876";
+update emp set hiredate="2020-05-27" where id="7900";
+update emp set hiredate="2021-05-27" where id="7902";
+update emp set hiredate="2022-05-27" where id="7934";
+-- option d end
